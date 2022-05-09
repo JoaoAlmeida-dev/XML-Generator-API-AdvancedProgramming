76e895c72120894992578c0abe6387f299e1a3b1
SoPlays <joao.ranger@gmail.com>
2022-05-09 17:31:45 +0100

    fix pom

--- src/test/core/controller/visitors/FilterVisitorTest.kt
+++ src/test/core/controller/visitors/FilterVisitorTest.kt
@@ -1,17 +1,15 @@
-package core.controller.visitors
+package controller.visitors
 
-import controller.visitors.FilterVisitor
 import model.Entity
 import model.XMLDocument
 import core.model.XmlHeader
-import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import testbed.model.Book
 import testbed.model.BookStore
 import testbed.model.Chapter
 import testbed.model.Library
+import kotlin.test.assertNotEquals
 
 internal class FilterVisitorTest {
     lateinit var xmldoc: XMLDocument
@@ -19,16 +17,15 @@ internal class FilterVisitorTest {
     @BeforeEach
     internal fun setUp() {
 
-        val book: Book =
-            Book(
-                author = "Jeronimo Stilton", pages = 1000, subTitle = "Aventuras", title = "Jeronimo em Belém",
-                store = BookStore.BERTRAND,
-                chapters = mutableListOf(
-                    Chapter(name = "Chapter 1", pageN = 20), Chapter(name = "Chapter 2", pageN = 40),
-                ),
-            )
+        val book = Book(
+            author = "Jeronimo Stilton", pages = 1000, subTitle = "Aventuras", title = "Jeronimo em Belém",
+            store = BookStore.BERTRAND,
+            chapters = mutableListOf(
+                Chapter(name = "Chapter 1", pageN = 20), Chapter(name = "Chapter 2", pageN = 40),
+            ),
+        )
 
-        val book2: Book = Book(
+        val book2 = Book(
             author = "Fernando Pessoa",
             pages = 200,
             subTitle = "Fernado",
@@ -39,13 +36,12 @@ internal class FilterVisitorTest {
             ),
         )
 
-        val library: Library =
-            Library(
-                stores = BookStore.values(),
-                title = "Livraria de Lisboa",
-                subTitle = "2022",
-                books = mutableListOf(book, book2)
-            )
+        val library = Library(
+            stores = BookStore.values(),
+            title = "Livraria de Lisboa",
+            subTitle = "2022",
+            books = mutableListOf(book, book2)
+        )
 
         val entity = Entity(obj = library, depth = 0)
         xmldoc = XMLDocument(entity = entity, header = XmlHeader(version = 1.0))
@@ -57,7 +53,7 @@ internal class FilterVisitorTest {
         val originalDocString = xmldoc.toString()
         xmldoc.accept(filterVisitor)
 
-        assertNotEquals(filterVisitor.document, originalDocString)
+        assertNotEquals(filterVisitor.document.toString(), originalDocString)
 
 
         println(filterVisitor.document)
